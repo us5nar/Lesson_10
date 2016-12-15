549acd099d39c79e996fb3fd405d75949e0c7c8f
Anton Bielov <us5nar@gmail.com>
2016-12-15 18:53:22 +0200

    Set structure

---
+++ LinkedList/LinkedList/LinkedList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class LinkedList<T>
+    {
+        public Node<T> Head { set; get; }
+        public Node<T> Last { set; get; }
+        public void Add(T NewElement)
+        {
+        }
+        public void Incert(T NewElement, int count) { }
+        public T Get(int count) { return Head; }
+    }
+}
---
+++ LinkedList/LinkedList/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Node<T>
+    {
+        public T Data { set; get; }
+        public Node<T> NextNode { set; get; }
+    }
+}
